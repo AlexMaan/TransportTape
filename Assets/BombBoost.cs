e6aa6e771a44d061c63634d10fcc6ced1ad86329
anovik <anovik@minsk.ximxim.com>
2017-05-05 15:10:19 +0300

    Bomb boost add

--- Assets/BombBoost.cs
+++ Assets/BombBoost.cs
@@ -4,25 +4,40 @@ using UnityEngine;
 
 public class BombBoost : MonoBehaviour {
 
-    List<GoodParam> effectedGoods;
+    public List<GoodParam> effectedGoods = new List<GoodParam>();
+    public List<BlockBox> effectedBoxes = new List<BlockBox>();
     float pressStapm;
     public float pressTime;
 
     void Awake() {
         GetComponent<CircleCollider2D>().radius = Booster.numberOfGoodsSelected - 1;
-        GetComponentInChildren<SpriteRenderer>().gameObject.transform.localScale *= 0.1f + 0.3f * Booster.numberOfGoodsSelected;
+        //GetComponentInChildren<SpriteRenderer>().gameObject.transform.localScale *= 2;
+        GetComponent<Animator>().SetTrigger("appear");
     }
 
     void OnTriggerEnter2D(Collider2D goodCollider) {
         effectedGoods.Add(goodCollider.GetComponent<GoodParam>());
+        effectedBoxes.Add(goodCollider.GetComponent<BlockBox>());
     }
 
     void OnMouseDrag() {
-        pressStapm += Time.deltaTime;
+        if(pressStapm == 0) GetComponent<Animator>().SetTrigger("grow");
+        pressStapm += Time.deltaTime;        
         if (pressStapm > pressTime) ExplodeBomb();
+    }
+    void OnMouseUp() {
+        pressStapm = 0;
+        GetComponent<Animator>().SetTrigger("grow_stop");
     }
 
     void ExplodeBomb() {
-        print("boom");
+        pressStapm = 0;        
+        foreach(GoodParam good in effectedGoods) {
+            if (good != null) { ProgressWheel.levelProgress += 0.5f; good.GetComponent<Animator>().SetTrigger("collect"); Destroy(good.gameObject, 0.4f); }
+        }
+        foreach (BlockBox box in effectedBoxes) {
+            if (box != null)  Destroy(box.gameObject, 0.1f);
+        }
+        Destroy(gameObject);
     }
 }
--- Assets/Booster.cs
+++ Assets/Booster.cs
@@ -4,20 +4,21 @@ using UnityEngine;
 
 public class Booster : MonoBehaviour {
 
+    public GameObject bombPrefab;
     public static int numberOfGoodsSelected;
 
     public void LaunchBoostEffect(int effectN, Vector3 effectPos, int collectedGoods) {
         numberOfGoodsSelected = collectedGoods;
         if (collectedGoods > 2) {
             switch (effectN) {
-                case 6: BombIt(effectPos);
+                case 6: StartCoroutine(BombIt(effectPos));
                     break;
                 case 0: PauseIt();
                     break;
                 case 1: goto case 0;
                 case 2: goto case 0;
                 case 3: goto case 0;
-                case 4: goto case 0;
+                case 4: goto case 6;
                 case 5: goto case 0;
                 default:
                     break;
@@ -25,8 +26,9 @@ public class Booster : MonoBehaviour {
         }
     }
 
-    void BombIt(Vector3 effectPos) {
-        print("bomb");
+    IEnumerator BombIt(Vector3 effectPos) {
+        yield return new WaitForSeconds(0.4f);
+        Instantiate(bombPrefab, effectPos, Quaternion.identity, GameObject.FindGameObjectWithTag("Tape").transform);
     }
 
     void PauseIt() {
