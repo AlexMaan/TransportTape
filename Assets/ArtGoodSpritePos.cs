9d3dd2ce1308a58defa87616716176ecf52a8c59
anovik <anovik@minsk.ximxim.com>
2017-05-30 15:07:11 +0300

    robot graphic + add

---
+++ Assets/ArtGoodSpritePos.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ArtGoodSpritePos : MonoBehaviour {
+
+    public float baseLine;
+    public float reposK;
+    float z;
+    Vector3 v;
+
+    void Awake() {
+        if (Random.Range(0f, 1f) > 0.5f) transform.localScale = new Vector3(-transform.localScale.x, transform.localScale.y, transform.localScale.z);
+
+        z = transform.position.z;
+        z += (baseLine + transform.position.y) * reposK;
+        transform.position = new Vector3(transform.position.x, transform.position.y, z);
+    }
+}
--- Assets/BombParam.cs
+++ Assets/BombParam.cs
@@ -6,11 +6,17 @@ public class BombParam : MonoBehaviour {
 
     public SpriteRenderer spriteRenderer;
     public GoodParam goodParam;
+    //float z;
+    //public float baseLine;
+    //public float reposK;
 
     void Start () {
         //spriteRenderer.color = Color.black;
         spriteRenderer.color = Color.white;
         goodParam.ColorIndex = 6;
-        
+
+        //z = transform.position.z;
+        //z += (baseLine + transform.position.y) * reposK;
+        //transform.position = new Vector3(transform.position.x, transform.position.y, z);
     }
 }
